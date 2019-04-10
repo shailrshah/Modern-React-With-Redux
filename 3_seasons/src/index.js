050916f2befb33ae7e804a5994f71fd6010ab128
Shail Shah <acad.shail@gmail.com>
2019-04-10 14:54:31 -0700

    Adds error handling

--- 3_seasons/src/index.js
+++ 3_seasons/src/index.js
@@ -1,28 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        //THIS IS THE ONLY TIME we directly assign state
-        this.state = {
-            latitude: null
-        };
+    //THIS IS THE ONLY TIME we directly assign state
+    this.state = {
+      latitude: null,
+      errorMessage: null
+    };
 
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-              this.setState({latitude: position.coords.latitude});
-            },
-            error => {
-              console.error(error);
-            }
-          );
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ latitude: position.coords.latitude });
+      },
+      error => {
+        this.setState({ errorMessage: error.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    } else if (this.state.latitude) {
+      return <div>Latitude: {this.state.latitude}</div>;
     }
-    render() {
-        return <div>Lattitude: {this.state.latitude}</div>;
-    }
+    return <div>Latitude: loading...</div>;
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
